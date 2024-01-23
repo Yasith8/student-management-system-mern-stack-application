dc5676b61f342c526439287f685e1801cf001a0f
Yasith <yasithnawodya9@gmail.com>
2024-01-23 10:29:37 +0530

    create student controller

--- backend/controller/studentController.js
+++ backend/controller/studentController.js
@@ -13,11 +13,13 @@ const getStudents = async(req, res) => {
 
 //create new student
 const addNewStudent = async(req, res) => {
+    //catch data come from request body
     const { studentNic, firstName, lastName, birthdate, gender, mobileNo, Address, email, isActive } = req.body
 
     //create empty array for check empty request
     let emptyFields = []
 
+    //if any field has empty push values to emptyField array
     if (!studentNic) {
         emptyFields.push('studentNic')
     }
@@ -48,15 +50,16 @@ const addNewStudent = async(req, res) => {
         emptyFields.push('isActive')
     }
 
+    //if array has data then show this error messege
     if (emptyFields.length > 0) {
         return res.status(400).json({ error: "Please fill in all the Field", emptyFields })
     }
 
-    // Convert birthdate to Date object
 
 
     //add document to database
     try {
+        // Convert birthdate to Date object
         const parsedBirthdate = new Date(birthdate);
         const student = await StudentModel.create({ studentNic, firstName, lastName, birthdate: parsedBirthdate, gender, mobileNo, Address, email, isActive })
         res.status(200).json(student)
@@ -65,9 +68,65 @@ const addNewStudent = async(req, res) => {
     }
 }
 
+//softdelete student
+const deleteStudent = async(req, res) => {
+    const { id } = req.parms
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).send("No Student with this ID")
+    }
+
+    const student = await Student.findByIdAndUpdate({ _id: id }, { isActive: false }, { new: true })
+
+    if (!student) {
+        return res.status(400).json("No Student with this ID")
+    }
+
+    res.status(200).json(student)
+}
+
+
+//hard delete  || delete data completely from db
+const removeStudent = async(req, res) => {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).send("No Student with this ID")
+    }
+
+    const student = await Student.findByIdAndDelete({ _id: id })
+
+    if (!student) {
+        return res.status(400).json("No student with this id")
+    }
+
+    res.status(200).json(student)
+}
+
+
+//update student vle status
+const studentStatus = async(req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).send("No Student with this ID")
+    }
+
+    const student = await Student.findByIdAndUpdate({ _id: id }, { isActive: true }, { new: true });
+
+    if (!student) {
+        return res.status(400).json("No student with this ID")
+    }
+
+    res.status(200).json(student);
+}
+
 
 
 module.exports = {
     getStudents,
-    addNewStudent
+    addNewStudent,
+    deleteStudent,
+    removeStudent,
+    studentStatus
 }
